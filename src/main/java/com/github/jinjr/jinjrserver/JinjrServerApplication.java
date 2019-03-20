80c9e7513f3d4f42cd6048031b92fe9c7c2b2864
Jinjr <jinjr.mi@gmail.com>
2019-03-20 08:03:51 +0800

    init

---
+++ src/main/java/com/github/jinjr/jinjrserver/JinjrServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jinjr.jinjrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinjrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinjrServerApplication.class, args);
+    }
+
+}

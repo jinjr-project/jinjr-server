285bc2243104f2ff89900b15a2641b477e0fdedb
Jinjr <jinjr.mi@gmail.com>
2019-03-22 09:39:23 +0800

    Enable CORS

---
+++ src/main/java/com/github/jinjr/jinjrserver/config/WebMvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.github.jinjr.jinjrserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(3600);
+    }
+
+}
